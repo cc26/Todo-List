7eb9ee175806bf0cba1867a2ec98ec85d8bccda4
cc26 <reynold8002@gmail.com>
2014-11-09 20:14:03 -0800

    add has_secure_password to generate attributes in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_secure_password
   attr_accessible :email, :name
   validates :name, presence: true
   email_rgex = /\A[\w+\-.]+@[\w\-.]+\.[a-z]+\z/i
@@ -8,6 +9,7 @@ class User < ActiveRecord::Base
 
   password_rgex = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6}$/
   validate :password, precense: true, format:{password_rgex}
+  validates :password_confirmation, presence: true
   before_save { |user| user.email = email.downcase }
 
 end
